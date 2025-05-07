8b87e360609d14744d7ff39ff6140bc982febf4f
Johnunist <JBord69@lsu.edu>
2025-05-07 15:07:51 -0500

    Update settings.tsx

--- settings.tsx
+++ settings.tsx
@@ -11,7 +11,7 @@ import {
 import { useRouter } from "expo-router";
 import { signOut } from "firebase/auth";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { auth } from "../app//firebaseConfig"; // Firebase instance
+import { auth } from "../app/firebaseConfig"; // Firebase auth instance
 
 // Main settings screen
 export default function SettingsScreen() {
