7d0027959c075c44527d0d14fc16df71b83fdc40
Johnunist <JBord69@lsu.edu>
2025-05-07 19:47:59 -0500

    Update firebaseConfig.ts

--- app/firebaseConfig.ts
+++ app/firebaseConfig.ts
@@ -4,7 +4,7 @@ import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// our web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyAn4h9O-bM2UEay0En5MvLGs7SesCyJRG0",
